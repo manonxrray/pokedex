366c9725cfa2f19913945799ddf826c6cdb394c2
Manon Gerray <Manon@MacBook-Pro-de-Manon.local>
2020-11-10 13:34:06 +0100

    Header styling ok :lipstick: :ok_hand:

--- src/components/LeftPart/Header.js
+++ src/components/LeftPart/Header.js
@@ -2,16 +2,19 @@ import React from 'react';
 
 const Header = () => (
   <div className="header">
-    <div className="radar">
-      <div className="bluePart">
-        <div className="light" />
+    <div className="left">
+      <div className="radar">
+        <div className="bluepart">
+          <div className="light" />
+        </div>
+      </div>
+      <div className="lights">
+        <div className="red small" />
+        <div className="yellow small" />
+        <div className="green small" />
       </div>
     </div>
-    <div className="lights">
-      <div className="red" />
-      <div className="yellow" />
-      <div className="green" />
-    </div>
+    <div className="right" />
   </div>
 );
 
