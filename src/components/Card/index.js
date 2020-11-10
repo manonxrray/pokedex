4abee14492183dcc5c210fa246af00e576275126
Manon Gerray <Manon@MacBook-Pro-de-Manon.local>
2020-11-10 15:28:57 +0100

    Pokemon Card styling :lipstick: :ok_hand:

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,7 +4,22 @@ import './card.scss';
 
 const Card = () => (
   <div className="card">
-    <h1>Composant : Card</h1>
+    <div className="image">
+      {/* an image will be displayed here */}
+    </div>
+    <div className="identity">
+      <div className="name">
+        <h2>Nom du Pokémon</h2>
+      </div>
+      <div className="types">
+        <div className="type">
+          <h4>ice</h4>
+        </div>
+        <div className="type">
+          <h4>fighting</h4>
+        </div>
+      </div> 
+    </div>
   </div>
 );
 
