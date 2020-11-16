3255aa1e011c2ee69fb5c497024ee3960ac939c6
Manon Gerray <manon.gerray@gmail.com>
2020-11-16 17:35:08 +0100

    Pokemon list styling :lipstick: :ok_hand:

---
+++ src/components/Screen/List.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const List = () => (
+  <div className="list">
+    <div className="item">
+      <h2 className="number">001.</h2>
+      <h2 className="name">Mr. Chibre</h2>
+    </div>
+    <div className="item">
+      <h2 className="number">001.</h2>
+      <h2 className="name">Mr. Chibre</h2>
+    </div>
+  </div>
+);
+
+export default List;
--- src/components/Screen/index.js
+++ src/components/Screen/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Card from 'src/components/Card';
+import List from './List';
 import './screen.scss';
 
 const Screen = () => (
   <div className="frame">
     <div className="border">
       <div className="screen">
+        <List />
       </div>
     </div>
   </div>
